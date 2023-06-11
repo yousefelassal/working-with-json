f46c655ef56fceb0399390fd93bf46abc825ff8a
Yousef <yousefsameh24@gmail.com>
2023-06-11 15:37:07 +0300

    populate header

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ async function populate() {
     populateHeader(superHeroes);
     populateHeroes(superHeroes);
 }
+
+function populateHeader(obj) {
+    const header = document.querySelector("header");
+    const myH1 = document.createElement("h1");
+    myH1.textContent = obj.squadName;
+    header.appendChild(myH1);
+  
+    const myPara = document.createElement("p");
+    myPara.textContent = `Hometown: ${obj.homeTown} // Formed: ${obj.formed}`;
+    header.appendChild(myPara);
+  }
+  
